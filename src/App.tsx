6dd9880975b9f7c438019a493f46ca9a951a4e6d
alina-kho <alina.khorolskaya0@gmail.com>
2022-10-20 16:21:53 +0200

    Routing added; Outlet for navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Movies } from "./pages/Movies";
+import { Characters } from "./pages/Characters";
+import { Paths } from "./models/Paths";
+
+//To make Navbar appear on all pages but the home one:
+const NavOutlet = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={Paths.HOME} element={<Home />} />
+        <Route element={<NavOutlet />}>
+          <Route path={Paths.MOVIES} element={<Movies />} />
+          <Route path={Paths.CHARACTERS} element={<Characters />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
